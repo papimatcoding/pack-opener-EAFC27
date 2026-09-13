98b0ab0912441e90fd5dc922a923efd832830535
papimatcoding <matalsan9@gmail.com>
2026-09-13 18:27:49 +0200

    v0.4: refresh PWA cache

--- sw.js
+++ sw.js
@@ -1,10 +1,5 @@
-const CACHE='packverse27-v3.0.0';
-const CORE=['./','./index.html','./css/app.css','./js/data.js','./js/app.js','./manifest.json','./assets/icon.svg'];
+const CACHE='packverse27-v4.0.0';
+const CORE=['./','./index.html','./v4/base.css','./v4/cards.css','./v4/app.css','./js/data.js','./v4/content.js','./v4/core.js','./v4/ui.js','./v4/game.js','./v4/main.js','./manifest.json','./assets/icon.svg'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
-self.addEventListener('fetch',event=>{
-  if(event.request.method!=='GET')return;
-  const url=new URL(event.request.url);
-  if(url.origin!==self.location.origin)return;
-  event.respondWith(caches.match(event.request).then(hit=>hit||fetch(event.request).then(response=>{if(response&&response.ok){const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy))}return response}).catch(()=>caches.match('./index.html'))));
-});
+self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;const url=new URL(event.request.url);if(url.origin!==self.location.origin)return;event.respondWith(fetch(event.request).then(response=>{if(response&&response.ok){const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy))}return response}).catch(()=>caches.match(event.request).then(hit=>hit||caches.match('./index.html'))))});
